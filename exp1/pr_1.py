43e55e3529de6c2c69c2c02bf21ede499a16ece8
unironictechguy <oktheekche@gmail.com>
2025-10-01 11:09:01 +0530

    add changes correct errors

--- exp1/pr_1.py
+++ exp1/pr_1.py
@@ -10,7 +10,7 @@ if marks >= 90:
     grade='A'
 elif marks>= 80:
     grade = 'B' 
-elif marks >- 70:
+elif marks >= 70:
     grade = 'C'
 elif marks >= 60:
     grade ='D'
@@ -19,5 +19,5 @@ else:
     
 #print the grade
 
-print("Your grade is: ")
+print("Your grade is: " ,grade)
     
