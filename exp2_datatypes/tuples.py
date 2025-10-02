8cadb9770b9f03d197e8c2e0134ce79dfe2d0c35
unironictechguy <oktheekche@gmail.com>
2025-10-02 15:59:18 +0530

    add tuples dt

---
+++ exp2_datatypes/tuples.py
@@ -0,0 +1,15 @@
+t = (1, 2, 3, 2, 4)
+print("Original tuple:", t)
+
+# Count & Index
+print("Count of 2:", t.count(2))
+print("Index of 3:", t.index(3))
+
+# Indexing & slicing
+print("First element:", t[0])
+print("Last element:", t[-1])
+print("Slice (1-4):", t[1:4])
+
+# Membership & length
+print("Is 3 in tuple?", 3 in t)
+print("Length of tuple:", len(t))
